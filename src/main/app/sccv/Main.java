83a416910fe40ecd9702b61a2957f15c5cc9e393
Thiago <thiago.silva@imd.ufrn.br>
2021-04-11 20:39:21 -0300

    :art: Adição da exibição da view no try/catch

--- src/main/app/sccv/Main.java
+++ src/main/app/sccv/Main.java
@@ -7,15 +7,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/menu/menu.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 678, 463));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("view/menu/menu.fxml"));
+            primaryStage.setTitle("Sistema de cadastro de compra e venda");
+            primaryStage.setScene(new Scene(root, 678, 463));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 
     public static void main(String[] args) {
         launch(args);
