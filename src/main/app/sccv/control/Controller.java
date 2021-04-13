e16874441f9aef6673831ede7bfe6166fbb906e9
Thiago <thiago.silva@imd.ufrn.br>
2021-04-13 20:18:41 -0300

    :construction: Trabalho em progresso
    - Atualização dos ids por fx:ids
    - Mudança no tipo do evento do método de MouseEvent para ActionEvent
    - Adição de método teste para salvar nome do cliente

--- src/main/app/sccv/control/Controller.java
+++ src/main/app/sccv/control/Controller.java
@@ -5,10 +5,13 @@ import main.app.sccv.model.payment.Payment;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
 
 public class Controller {
   // Client
+  @FXML
+  private TableColumn<?, ?> columnClientName;
   @FXML
   private TextField inputClientName;
 
@@ -44,111 +47,117 @@ public class Controller {
 
   // Handle Client
   @FXML
-  public void handleSaveClient(MouseEvent mouseEvent) {
-    System.out.println("Botão salvar cliente funcionando!");
+  public void handleSaveClientName(ActionEvent event) {
+    try {
+      if ((inputClientName.getText() != null && !inputClientName.getText().isEmpty())) {
+        columnClientName.setText(inputClientName.getText());
+      }
+    } catch (Exception e) {
+      System.err.println("Error: " + e + "não pode ficar vazio.");
+    }
   }
 
   @FXML
-  public void handleEditClient(MouseEvent mouseEvent) {
+  public void handleEditClient(ActionEvent event) {
     System.out.println("Botão editar cliente funcionando!");
   }
 
   @FXML
-  public void handleCancelClient(MouseEvent mouseEvent) {
+  public void handleCancelClient(ActionEvent event) {
     System.out.println("Botão cancelar cliente funcionando!");
   }
 
   @FXML
-  public void handleDeleteClient(MouseEvent mouseEvent) {
+  public void handleDeleteClient(ActionEvent event) {
     System.out.println("Botão apagar cliente funcionando!");
   }
 
   @FXML
-  public void handleListClient(MouseEvent mouseEvent) {
+  public void handleListClient(ActionEvent event) {
     System.out.println("Botão listar cliente funcionando!");
   }
 
   // Handle Seller
   @FXML
-  public void handleSaveSeller(MouseEvent mouseEvent) {
+  public void handleSaveSeller(ActionEvent event) {
     System.out.println("Botão salvar vendedor funcionando!");
   }
 
   @FXML
-  public void handleEditSeller(MouseEvent mouseEvent) {
+  public void handleEditSeller(ActionEvent event) {
     System.out.println("Botão editar vendedor funcionando!");
   }
 
   @FXML
-  public void handleCancelSeller(MouseEvent mouseEvent) {
+  public void handleCancelSeller(ActionEvent event) {
     System.out.println("Botão cancelar vendedor funcionando!");
   }
 
   @FXML
-  public void handleDeleteSeller(MouseEvent mouseEvent) {
+  public void handleDeleteSeller(ActionEvent event) {
     System.out.println("Botão apagar vendedor funcionando!");
   }
 
   @FXML
-  public void handleListSeller(MouseEvent mouseEvent) {
+  public void handleListSeller(ActionEvent event) {
     System.out.println("Botão listar vendedor funcionando!");
   }
 
   // Handle Product
   @FXML
-  public void handleSaveProduct(MouseEvent mouseEvent) {
+  public void handleSaveProduct(ActionEvent event) {
     System.out.println("Botão salvar produto funcionando!");
   }
 
   @FXML
-  public void handleEditProduct(MouseEvent mouseEvent) {
+  public void handleEditProduct(ActionEvent event) {
     System.out.println("Botão editar produto funcionando!");
   }
 
   @FXML
-  public void handleCancelProduct(MouseEvent mouseEvent) {
+  public void handleCancelProduct(ActionEvent event) {
     System.out.println("Botão cancelar produto funcionando!");
   }
 
   @FXML
-  public void handleDeleteProduct(MouseEvent mouseEvent) {
+  public void handleDeleteProduct(ActionEvent event) {
     System.out.println("Botão apagar produto funcionando!");
   }
 
   @FXML
-  public void handleListProduct(MouseEvent mouseEvent) {
+  public void handleListProduct(ActionEvent event) {
     System.out.println("Botão listar produto funcionando!");
   }
 
   // Handle Sell
   @FXML
-  public void handleSaveSell(MouseEvent mouseEvent) {
+  public void handleSaveSell(ActionEvent event) {
     System.out.println("Botão salvar venda funcionando!");
   }
 
   @FXML
-  public void handleEditSell(MouseEvent mouseEvent) {
+  public void handleEditSell(ActionEvent event) {
     System.out.println("Botão editar venda funcionando!");
   }
 
   @FXML
-  public void handleCancelSell(MouseEvent mouseEvent) {
+  public void handleCancelSell(ActionEvent event) {
     System.out.println("Botão cancelar produto funcionando!");
   }
 
   @FXML
-  public void handleDeleteSell(MouseEvent mouseEvent) {
+  public void handleDeleteSell(ActionEvent event) {
     System.out.println("Botão apagar produto funcionando!");
   }
 
   @FXML
-  public void handleListSell(MouseEvent mouseEvent) {
+  public void handleListSell(ActionEvent event) {
     System.out.println("Botão listar venda funcionando!");
   }
 
   // Payment Method
   @FXML
-  public void handleSelectPaymentMethod(MouseEvent mouseEvent) {
+  public void handleSelectPaymentMethod(ActionEvent event) {
     System.out.print("\t1 - Pix\n\t2 - Débito\n\t3 - Crédito\n\t4 - Boleto\nSelecione a forma de pagamento: ");
   }
 }
