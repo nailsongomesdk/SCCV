d3e631b2c605dae119db3544eb0acb8af58543ef
Thiago <thiago.silva@imd.ufrn.br>
2021-04-15 15:31:55 -0300

    :pencil2: Atualizar indentação do código

--- src/main/app/sccv/Main.java
+++ src/main/app/sccv/Main.java
@@ -7,19 +7,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("view/menu/menu.fxml"));
-            primaryStage.setTitle("Sistema de cadastro de compra e venda");
-            primaryStage.setScene(new Scene(root, 800, 600));
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    public static void main(String[] args) {
-        launch(args);
+  @Override
+  public void start(Stage primaryStage) {
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("view/menu/menu.fxml"));
+      primaryStage.setTitle("Sistema de cadastro de compra e venda");
+      primaryStage.setScene(new Scene(root, 800, 600));
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 }
